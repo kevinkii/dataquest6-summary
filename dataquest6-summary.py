d94115f0b2058a8740aff2e867a986aa1cb4bdf4
kevinkii <kevinmtaing@Monireangseys-MacBook-Pro.local>
2019-10-08 16:00:23 -0700

    wrap up

--- dataquest6-summary.py
+++ dataquest6-summary.py
@@ -325,9 +325,211 @@ pivot_table_combined.plot(kind ='barh', title='Mean Happiness Scores by Year', x
 #%% [markdown]
 # ## Working With Strings In Pandas
 
+# | Method | Description |
+# | --- | ---|
+# | Series.str.split()|Splits each element in the Series.|
+# | Series.str.strip()|Strips whitespace from each string in the Series.|
+# | Series.str.lower()|Converts strings in the Series to lowercase.|
+# | Series.str.upper()|Converts strings in the Series to uppercase.|
+# | Series.str.get()|Retrieves the ith element of each element in the Series.|
+# | Series.str.replace()|Replaces a regex or string in the Series with another string.|
+# | Series.str.cat()|Concatenates strings in a Series.|
+# | Series.str.extract()|Extracts substrings from the Series matching a regex pattern.|
+
+# Find specific strings or substrings in a column:
+#
+# `df[col_name].str.contains(pattern)`
+#
+# Extract specific strings or substrings in a column:
+#
+# `df[col_name].str.extract(pattern)`
+#
+# Extract more than one group of patterns from a column:
+#
+# `df[col_name].str.extractall(pattern)`
+#
+# Replace a regex or string in a column with another string:
+#
+# `df[col_name].str.replace(pattern, replacement_string)`
+
 #%%
 world_dev = pd.read_csv("World_dev.csv")
 col_renaming = {'SourceOfMostRecentIncomeAndExpenditureData': 'IESurvey'}
 world_dev = world_dev.rename(col_renaming, axis = 1)
 
 merged = pd.merge(left=happiness2015, right=world_dev, how='left', left_on=happiness2015['Country'], right_on=world_dev['ShortName'])
+
+#%%
+def extract_last_word(element):
+    element = str(element)
+    element = element.split()
+    return element[-1]
+
+merged['Currency Apply'] = merged['CurrencyUnit'].apply(extract_last_word)
+merged['Currency Apply'].head(5)
+
+#%%
+merged['Currency Vectorized'] = merged['CurrencyUnit'].str.split().str.get(-1)
+merged['Currency Vectorized'].head(5)
+
+#%%
+lengths = merged['CurrencyUnit'].str.len()
+value_counts = lengths.value_counts(dropna=False)
+
+# return the length of each currency unit
+def lengths_str(element):
+    return len(str(element))
+lengths_apply1 = merged['CurrencyUnit'].apply(lengths_str)
+
+# exclude the missing values
+def lengths_missing(element):
+    if pd.isnull(element):
+        pass
+    else:
+        return len(str(element))
+lengths_apply2 = merged['CurrencyUnit'].apply(lengths_missing)
+
+#%%
+pattern = r"[Nn]ational accounts"
+national_accounts = merged['SpecialNotes'].str.contains(pattern)
+national_accounts.head(5)
+
+#%%
+#Return the value counts for each value in the Series, including missing values.
+pattern = r"[Nn]ational accounts"
+national_accounts = merged['SpecialNotes'].str.contains(pattern, na=False)
+merged_national_accounts = merged[national_accounts]
+merged_national_accounts.head()
+
+#%%
+pattern = r"([1-2][0-9]{3})"
+pattern =r"([0-9]{4})"
+years = merged['SpecialNotes'].str.extract(pattern)
+
+#%%
+# syntax `(?P<Column_Name>...)`
+# caturing the column `Years`
+pattern = r"(?P<Years>[1-2][0-9]{3})"
+years = merged['IESurvey'].str.extractall(pattern)
+value_counts = years['Years'].value_counts()
+print(value_counts)
+
+#%%
+pattern = r"(?P<First_Year>[1-2][0-9]{3})/?(?P<Second_Year>[0-9]{2})?"
+years = merged['IESurvey'].str.extractall(pattern)
+first_two_year = years['First_Year'].str[0:2]
+years['Second_Year'] = first_two_year + years['Second_Year']
+
+#%%
+import matplotlib.pyplot as plt
+merged['IncomeGroup'] = merged['IncomeGroup'].str.replace(' income', '').str.replace(':', '').str.upper()
+pv_incomes = merged.pivot_table(values='Happiness Score', index='IncomeGroup')
+pv_incomes.plot(kind='bar', rot=30, ylim=(0,10))
+plt.show()
+
+#%% [markdown]
+# ## Working With Missing And Duplicate Data
+# Although there is no perfect way to handle missing values and each situation is different, now we know the basic techniques and built some intuition around them to better inform our decisions. Below is the workflow we used to clean missing values:
+
+# * Check for errors in data cleaning/transformation.
+# * Use data from additional sources to fill missing values.
+# * Drop row/column.
+# * Fill missing values with reasonable estimates computed from the available data.
+#
+#
+# We also started to set a more defined data cleaning workflow, in which we:
+# * Set a goal for the project.
+# * Researched and tried to understand the data.
+# * Determined what data was needed to complete our analysis.
+# * Added columns.
+# * Cleaned specific data types.
+# * Combined data sets.
+# * Removed duplicate values.
+# * Handled the missing values.
+
+#%%
+# find the total missing value in each dataset
+missing_2015 = happiness2015.isnull().sum()
+missing_2016 = happiness2016.isnull().sum()
+missing_2017 = happiness2017.isnull().sum()
+
+#%%
+# cleaning the name of the column
+happiness2017.columns = happiness2017.columns.str.replace('.', ' ').str.replace('\s+', ' ').str.strip().str.upper()
+happiness2016.columns = happiness2016.columns.str.replace(r'[\(\)]', '').str.strip().str.upper()
+happiness2015.columns = happiness2015.columns.str.replace(r'[\(\)]', '').str.strip().str.upper()
+
+
+combined = pd.concat([happiness2015, happiness2016, happiness2017], ignore_index = True)
+missing = combined.isnull().sum()
+print(missing)
+
+#%%
+# graph the location of the missing value
+import seaborn as sns
+combined_updated = combined.set_index('YEAR')
+sns.heatmap(combined_updated.isnull(), cbar=False)
+
+#%% [markdown]
+# We can make the following observations:
+# * No values are missing in the `COUNTRY` column.
+# * There are some rows in the 2015, 2016, and 2017 data with missing values in all columns EXCEPT the `COUNTRY` column.
+# * Some columns only have data populated for one year.
+# * It looks like the `REGION` data is missing for the year 2017.
+
+#%%
+regions_2017 = combined[combined['YEAR'] == 2017]['REGION']
+missing_region = regions_2017.isnull().sum()
+print(missing_region)
+
+#%%
+combined = pd.merge(left = combined, right = regions, on = 'COUNTRY', how = 'left')
+combined = combined.drop('REGION_x', axis = 1)
+combined.isnull().sum()
+
+#%%
+# finding the duplication
+combined['COUNTRY'] = combined['COUNTRY'].str.upper()
+dups = combined.duplicated(['COUNTRY','YEAR'])
+combined[dups]
+
+#%%
+# dropping the duplication
+combined['COUNTRY'] = combined['COUNTRY'].str.upper()
+combined = combined.drop_duplicates(['COUNTRY','YEAR'])
+
+#%%
+# dropping unused column
+columns_to_drop = ['LOWER CONFIDENCE INTERVAL', 'STANDARD ERROR', 'UPPER CONFIDENCE INTERVAL', 'WHISKER HIGH', 'WHISKER LOW']
+
+combined = combined.drop(columns_to_drop,axis = 1)
+# dropping any columns that have less than 159 non null value
+combined = combined.dropna(thresh = 159, axis = 1)
+
+#%%
+sorted = combined.set_index('REGION').sort_values(['REGION', 'HAPPINESS SCORE'])
+sns.heatmap(sorted.isnull(), cbar=False)
+
+#%% [markdown]
+# From the graph, we confirmed:
+# * Only about 4 percent of the values in each column are missing.
+# * Dropping rows with missing values won't cause us to lose information in other columns.
+#
+#
+# There are many options for choosing the replacement value, including:
+# * A constant value
+# * The mean of the column
+# * The median of the column
+# * The mode of the column
+
+#%%
+happiness_mean = combined['HAPPINESS SCORE'].mean()
+print(happiness_mean)
+
+combined['HAPPINESS SCORE UPDATED'] = combined['HAPPINESS SCORE'].fillna(happiness_mean)
+
+# the replacing missing values with the Series mean doesn't change the mean of the Series.
+print(combined['HAPPINESS SCORE UPDATED'].mean())
+
+#%%
+combined = combined.dropna()
